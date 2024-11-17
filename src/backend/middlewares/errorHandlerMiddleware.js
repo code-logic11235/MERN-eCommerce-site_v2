81f70925c8c6a26630fa3722397e65c8926572e2
Tai Pham <tai.pham1738@gmail.com>
2024-11-16 22:35:56 -0600

    handled error

--- src/backend/middlewares/errorHandlerMiddleware.js
+++ src/backend/middlewares/errorHandlerMiddleware.js
@@ -16,6 +16,24 @@ export default (err, req, res, next) =>{
         const message = Object.values(err.errors).map((value) => value.message)
         error = new ErrorHandler(message, 404);
     }
+
+    // handle invalid mongoose duplicate key
+    if(err.code === 11000){
+        const message = `Duplicate ${Object.keys(err.keyValue)} entered`
+        error = new ErrorHandler(message, 404);
+    }
+    // Handle wrong JWT Error
+    if (err.name === "JsonWebTokenError") {
+    const message = `JSON Web Token is invalid. Try Again`;
+    error = new ErrorHandler(message, 400);
+    }
+    // Handle expired JWT Error
+    if (err.name === "TokenExpiredError") {
+    const message = `JSON Web Token is expired. Try Again`;
+    error = new ErrorHandler(message, 400);
+    }
+
+
     if (process.env.NODE_ENV === "DEVELOPMENT") {
         res.status(error.statusCode).json({
             message: error.message,
