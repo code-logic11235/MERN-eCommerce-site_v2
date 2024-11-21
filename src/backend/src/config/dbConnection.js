937b8e155518ecfb6914c4e69c1c5d3fadf6e4d2
Tai Pham <tai.pham1738@gmail.com>
2024-11-20 21:09:38 -0600

    updated connection for Mongoose

--- src/backend/src/config/dbConnection.js
+++ src/backend/src/config/dbConnection.js
@@ -4,14 +4,12 @@ export const connectDatabase = () =>{
 
     let DB_URI="";
     if(process.env.NODE_ENV === 'DEVELOPMENT'){
-        console.log("HIT213231421431")
         DB_URI = process.env.DB_LOCAL_URI;
     }else if(process.env.NODE_ENV === 'PRODUCTION'){
         DB_URI = process.env.DB_URI;  
     } else {
         DB_URI = process.env.DB_LOCAL_URI;
     }
-    console.log('-----------------------------', process.env.DB_LOCAL_URI)
     mongoose.connect(DB_URI)
     .then((con)=>{
         console.log('MongoDB Database Connected with Host:',con?.connection?.host)
