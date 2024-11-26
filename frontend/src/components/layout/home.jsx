da174990777c7363e715f7f1800268001ef5e799
Tai Pham <tai.pham1738@gmail.com>
2024-11-25 18:14:22 -0600

    added filters

--- frontend/src/components/layout/home.jsx
+++ frontend/src/components/layout/home.jsx
@@ -6,12 +6,16 @@ import Loader from "./loader";
 import toast from "react-hot-toast";
 import CustomPagination from "./customPagination";
 import { useSearchParams } from "react-router-dom";
+// import Filters from "./filters";
 
 const Home = () =>{
 
     let [searchParams] = useSearchParams();
     const page = searchParams.get("page") || 1; 
-    const params = {page}; // turn it into params obj to pass to our query
+    const keyword = searchParams.get("keyword") || ""; 
+
+    const params = {page, keyword}; // turn it into params obj to pass to our query
+
 
     const {data, isLoading, error, isError} = useGetProductsQuery( params);
     useEffect(()=>{
@@ -19,28 +23,38 @@ const Home = () =>{
             toast.error(error?.data?.message)
         }
     },[isError])
-
+    
     if(isLoading) return <Loader/>
-    return (
-        <>
-            <MetaData title={"buy Best Products Online"}/>
-            <div className="container">
-                <div className="row">
-                    <div className="col-12 col-sm-6 col-md-12">
-                        <h1 id="products_heading" className="text-secondary">Latest Products</h1>
-                        <section id="products" className="mt-5">
-                            <div className="row">
+    const columnSize = keyword ? 4 : 3;
 
-                                {data?.products?.map((product, key)=>(
-                                    <ProductItem product={product} key={key}/>
-                                ))}
-                            </div>
-                        </section>
-                        <CustomPagination resPerPage={data?.itemPerPage} filteredProductsCount={data?.productCount}/>
-                    </div>
-                </div>
+    return (
+          <>
+      <MetaData title={"Buy Best Products Online"} />
+      <div className="row">
+        {keyword && (
+          <div className="col-6 col-md-3 mt-5">
+            {/* <Filters /> */}
+          </div>
+        )}
+        <div className={keyword ? "col-6 col-md-9" : "col-6 col-md-12"}>
+          <h1 id="products_heading" className="text-secondary">
+            {keyword
+              ? `${data?.productCount} Products found with keyword: ${keyword}`
+              : "Latest Products"}
+          </h1>
+
+          <section id="products" className="mt-5">
+            <div className="row">
+              {data?.products?.map((product) => (
+                <ProductItem product={product} columnSize={columnSize} />
+              ))}
             </div>
-        </>
+          </section>
+
+          <CustomPagination resPerPage={data?.itemPerPage} filteredProductsCount={data?.productCount}/>
+        </div>
+      </div>
+    </>
     )
 }
 
