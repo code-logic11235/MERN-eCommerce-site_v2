8f5c81589a0f8979d73b0d2aab0edf78eff50c60
Tai Pham <tai.pham1738@gmail.com>
2024-12-06 16:07:57 -0700

    added comments

--- backend/src/utils/cloudinary.js
+++ backend/src/utils/cloudinary.js
@@ -1,34 +1,32 @@
-import cloudinary from 'cloudinary';
-import dotenv from 'dotenv'
+import cloudinary from "cloudinary";
+import dotenv from "dotenv";
 
-dotenv.config({ path: 'src/config/config.env' }); 
+dotenv.config({ path: "src/config/config.env" });
 
 cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+export const upload_file = (file, folder) => {
+  return new Promise((res, rej) => {
+    cloudinary.uploader.upload(
+      file, // The file to upload (either from local storage or base64)
+      (result) => {
+        res({
+          public_id: result.public_id, // The unique identifier assigned by Cloudinary
+          url: result.url, // The publicly accessible URL of the uploaded image
+        });
+      },
+      {
+        resource_type: "auto", // Automatically determines the file type (image, video, etc.)
+        folder, // Specifies the folder in Cloudinary to upload the file to
+      }
+    );
+  });
+};
+export const delete_file = async (file) => {
+  const res = await cloudinary.uploader.destroy(file);
 
-})
-export const upload_file = (file, folder) =>{
-    
-    return new Promise((res, rej)=>{
-        cloudinary.uploader.upload(
-            file,
-            (result) =>{
-                res({
-                    public_id: result.public_id,
-                    url: result.url,
-                });
-            }, 
-            {
-                resource_type: 'auto',
-                folder
-            }
-        )
-    })
-}
-export const delete_file = async (file) =>{
-    const res = await cloudinary.uploader.destroy(file)
-
-    if(res?.result === "ok") return true
-}
+  if (res?.result === "ok") return true;
+};
