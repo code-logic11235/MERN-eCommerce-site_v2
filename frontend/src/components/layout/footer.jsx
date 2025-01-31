adab270bcde4a252e5761191c10ba9841f58dd86
Tai Pham <tai.pham1738@gmail.com>
2025-01-30 22:07:53 -0700

    updated footer content and css

--- frontend/src/components/layout/footer.jsx
+++ frontend/src/components/layout/footer.jsx
@@ -1,12 +1,35 @@
 import React from "react";
-
+import "./footer.css"
 const Footer = () =>{
     return (
               
-        <footer className=" homepage-footer py-1 pt-5">
-        <p className="text-center mt-1 fw-bold">
-            ShopHaven - 2020-2023, All Rights Reserved
-        </p>
+        <footer className="footer">
+            
+            <div className="footer-container">
+                <div className="footer-logo">L'Essence </div>
+
+                <div className="footer-links-container"> 
+                    <h5 className="footer-about">
+                        <span> About</span>
+                        <a className="footer-about-link">L'Essence</a>
+                        <a className="footer-about-link2">Developer</a>
+                    </h5>
+                    <h5 className="footer-follow">
+                        <span>Folow</span>
+                        <a className="footer-folow-github">GitHub</a>
+                        <a className="footer-folow-linkedIn">Linked In</a>
+                    </h5>
+                    <h5 className="footer-legal">
+                        <span>Legal</span>
+                        <a className="footer-legal-privacy">Privacy Policy</a>
+                        <a className="footer-legal-terms">Terms & Conditions</a>
+                    </h5>
+                </div>
+            </div>
+            <hr />
+            <div className="footer-social-links" >
+                <span>2024 Tai Pham</span>
+            </div>
         </footer>
 
 
