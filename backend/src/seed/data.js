e0a6acb02b1a27d2fafc91db502e11e167ba01f4
Tai Pham <tai.pham1738@gmail.com>
2025-02-09 15:28:21 -0700

    changed seed data

--- backend/src/seed/data.js
+++ backend/src/seed/data.js
@@ -1,220 +1,270 @@
 export default [
   {
-    name: "SanDisk Ultra 128GB SDXC UHS-I Memory Card up to 80MB/s",
-    price: 45.89,
+    name: "Lavande 42",
+    price: 16.99,
     description:
-      "Ultra-fast cards (2) to take better pictures and Full HD videos (1) with your compact to mid-range point-and-shoot cameras and camcorders. With SanDisk Ultra SDXC UHS-I cards you’ll benefit from faster downloads, high capacity, and better performance to capture and store 128GB (5) of high quality pictures and Full HD video (1). Take advantage of ultra-fast read speeds of up to 80MB/s (3) to save time moving photos and videos from the card to your computer. From a leader in flash memory storage, SanDisk Ultra SDXC UHS-I cards are compatible with SDHC and SDXC digital devices, and come with a 10-year limited warranty (6).",
+      "A calming and serene fragrance, this lavender scent envelops the senses with its soothing floral notes. Perfect for relaxation, it brings the essence of a peaceful garden into your home, creating a tranquil and comforting atmosphere.",
     ratings: 4.5,
     images: [
       {
-        public_id: "shopit/demo/nkkjkta63uiazppzkmjf",
-        url: "http://res.cloudinary.com/udemy-courses/image/upload/v1698577488/shopit/demo/nkkjkta63uiazppzkmjf.jpg",
+        url: "https://res.cloudinary.com/taipham232/image/upload/v1737498983/ShopHaven/products/bottle-skinny-2.0_hsehjh.webp",
       },
       {
-        public_id: "shopit/demo/hz1iwdqzrvxtfxincvju",
-        url: "http://res.cloudinary.com/udemy-courses/image/upload/v1698577488/shopit/demo/hz1iwdqzrvxtfxincvju.jpg",
+        url: "https://res.cloudinary.com/taipham232/image/upload/v1737498983/ShopHaven/products/bottle-skinny-2.1_cexhvy.webp",
       },
-      {
-        public_id: "shopit/demo/oc2dvvkxyxukc13y9pjv",
-        url: "http://res.cloudinary.com/udemy-courses/image/upload/v1698577488/shopit/demo/oc2dvvkxyxukc13y9pjv.jpg",
-      },
+      
     ],
-    category: "Electronics",
-    seller: "Ebay",
-    stock: 50,
-    numOfReviews: 32,
+    category: "Fragrance",
+    stock: 10,
+    numOfReviews: 0,
     reviews: [],
   },
   {
-    name: "CAN USB FD Adapter (GC-CAN-USB-FD)",
-    price: 315,
+    name: "Noir 12",
+    price: 48.99,
     description:
-      "Monitor a CAN network, write a CAN program and communicate with industrial, medical, automotive or other CAN based device. Connect CAN FD and CAN networks to a computer via USB with the CAN USB FD adapter.",
-    ratings: 1.65,
+      "A elegant and mysterious fragrance that exudes depth and sophistication. With bold notes of rich spices, dark florals, and a touch of smoky undertones, this captivating scent evokes a sense of intrigue and allure, perfect for the modern, confident individual.",
+    ratings: 4.6,
     images: [
       {
-        public_id: "shopit/demo/e3hweb3tbp5zacfi564c",
-        url: "http://res.cloudinary.com/udemy-courses/image/upload/v1698577509/shopit/demo/e3hweb3tbp5zacfi564c.jpg",
+        url: "https://res.cloudinary.com/taipham232/image/upload/v1737498983/ShopHaven/products/bottle-4.0_kgykha",
       },
       {
-        public_id: "shopit/demo/mjcc0kg0lzxegrauh8qc",
-        url: "http://res.cloudinary.com/udemy-courses/image/upload/v1698577509/shopit/demo/mjcc0kg0lzxegrauh8qc.jpg",
+        url: "https://res.cloudinary.com/taipham232/image/upload/v1737498983/ShopHaven/products/bottle-4.1_h4spp1",
       },
-      {
-        public_id: "shopit/demo/iruzmpgrhb6xyqheppxg",
-        url: "http://res.cloudinary.com/udemy-courses/image/upload/v1698577509/shopit/demo/iruzmpgrhb6xyqheppxg.jpg",
-      },
-      {
-        public_id: "shopit/demo/bj3auijqcxxvkwdohznb",
-        url: "http://res.cloudinary.com/udemy-courses/image/upload/v1698577510/shopit/demo/bj3auijqcxxvkwdohznb.jpg",
-      },
+  
     ],
-    category: "Electronics",
-    seller: "Amazon",
-    stock: 0,
-    numOfReviews: 2,
+    category: "Fragrance",
+    stock: 10,
+    numOfReviews: 0,
     reviews: [],
   },
   {
-    name: "CHARMOUNT Full Motion TV Wall Mount Swivel",
-    price: 26.99,
+    name: "Rosé",
+    price: 48.99,
     description:
-      "CHARMOUNT TV MOUNT UNIVERSAL DESIGN - Has your TV been received? Tilted TV wall mount is for 26 - 55 TVs weight up to 88lbs 40 kg. Our tilt TV mount has a compatible faceplate that fits VESA 75X75mm (3x3). CHARMOUNT TV MOUNT UNIVERSAL DESIGN - Has your TV been received? Tilted TV wall mount is for 26 - 55 TVs weight up to 88lbs 40 kg. Our tilt TV mount has a compatible faceplate that fits VESA 75X75mm (3x3) CHARMOUNT TV MOUNT UNIVERSAL DESIGN - Has your TV been received? Tilted TV wall mount is for 26 - 55 TVs weight up to 88lbs 40 kg. Our tilt TV mount has a compatible faceplate that fits VESA 75X75mm (3x3).",
+      "Delicate and refreshing fragrance that captures the essence of blooming roses in full bloom. With soft floral notes and a hint of fruity sweetness, it evokes the elegance of a sun-kissed garden, creating a light, romantic, and timeless scent",
     ratings: 3.5,
     images: [
       {
-        public_id: "shopit/demo/yqqwxrgst2pi3frtngaw",
-        url: "http://res.cloudinary.com/udemy-courses/image/upload/v1698577531/shopit/demo/yqqwxrgst2pi3frtngaw.jpg",
+        url: "https://res.cloudinary.com/taipham232/image/upload/v1737498983/ShopHaven/products/bottle-2.0_h2zw0p",
       },
       {
-        public_id: "shopit/demo/pgcwny8xyshsnwaiikbm",
-        url: "http://res.cloudinary.com/udemy-courses/image/upload/v1698577531/shopit/demo/pgcwny8xyshsnwaiikbm.jpg",
+        url: "https://res.cloudinary.com/taipham232/image/upload/v1737498983/ShopHaven/products/bottle-2.1_qqcm6u",
       },
     ],
-    category: "Electronics",
-    seller: "Amazon",
-    stock: 1,
-    numOfReviews: 12,
+    category: "Fragrance",
+    stock: 5,
+    numOfReviews: 0,
     reviews: [],
   },
   {
-    name: "Bose QuietComfort 35 II Wireless Bluetooth Headphones",
-    price: 299,
+    name: "Mystic No. 4",
+    price: 52.99,
     description:
-      "What happens when you clear away the noisy distractions of the world? Concentration goes to the next level. You get deeper into your music, your work, or whatever you want to focus on. That’s the power of Bose QuietComfort 35 wireless headphones II. Put them on and get closer to what you’re most passionate about. And that’s just the beginning. QuietComfort 35 wireless headphones II are now enabled with Bose AR",
+      "An enchanting fragrance that transports you to a world of mystery and allure. With an intriguing blend of warm spices, deep woods, and subtle floral notes, it creates a captivating aura that lingers, inviting curiosity and leaving a lasting impression.",
     ratings: 4.5,
     images: [
       {
-        public_id: "shopit/demo/yxbrklp7snr7dgxfrxu9",
-        url: "http://res.cloudinary.com/udemy-courses/image/upload/v1698577543/shopit/demo/yxbrklp7snr7dgxfrxu9.jpg",
+        url: "https://res.cloudinary.com/taipham232/image/upload/v1737498983/ShopHaven/products/bottle-3.0_nj9gye",
       },
-      {
-        public_id: "shopit/demo/i8ge1ilin2fz9tk2okni",
-        url: "http://res.cloudinary.com/udemy-courses/image/upload/v1698577543/shopit/demo/i8ge1ilin2fz9tk2okni.jpg",
-      },
+
     ],
-    category: "Headphones",
-    seller: "Amazon",
+    category: "Fragrance",
+
     stock: 11,
-    numOfReviews: 112,
+    numOfReviews: 0,
     reviews: [],
   },
   {
-    name: "Apple AirPods with Charging Case (Wired)",
-    price: 126.99,
+    name: "Neroli 57",
+    price: 48.99,
     description:
-      "AirPods with Charging Case: More than 24 hours listening time, up to 18 hours talk time; AirPods (single charge): Up to 5 hours listening time, up to 3 hours talk time or 15 minutes in the case equals up to 3 hours listening time or up to 2 hours talk time.",
+      "A bright and uplifting fragrance that celebrates the crisp, citrusy essence of neroli blossoms. With its fresh floral notes and a touch of sweetness, it exudes a sense of warmth and elegance, evoking the feeling of a sunlit afternoon in a blooming orchard.",
     ratings: 4.8,
     images: [
       {
-        public_id: "shopit/demo/yj00oalanbzmbzctsbln",
-        url: "http://res.cloudinary.com/udemy-courses/image/upload/v1698577557/shopit/demo/yj00oalanbzmbzctsbln.jpg",
+        url: "https://res.cloudinary.com/taipham232/image/upload/v1737498983/ShopHaven/products/bottle-6.0_nkv3uw",
       },
-      {
-        public_id: "shopit/demo/mijzcdd5sle8a2any1i8",
-        url: "http://res.cloudinary.com/udemy-courses/image/upload/v1698577557/shopit/demo/mijzcdd5sle8a2any1i8.jpg",
-      },
-      {
-        public_id: "shopit/demo/kxckb0o7fbdwqgmugoct",
-        url: "http://res.cloudinary.com/udemy-courses/image/upload/v1698577557/shopit/demo/kxckb0o7fbdwqgmugoct.jpg",
-      },
+
     ],
-    category: "Electronics",
-    seller: "Amazon",
-    stock: 122,
-    numOfReviews: 2671,
+    category: "Fragrance",
+
+    stock: 50,
+    numOfReviews: 0,
     reviews: [],
   },
   {
-    name: "Cable Boom Microphone - Volume Control for Playstation PS4 or Xbox",
-    price: 27.99,
+    name: "Orange Fluer",
+    price: 38.99,
     description:
-      "DESIGN INFO - 3.5mm male to 2.5mm male audio cable adapter with Upgraded Flexible, Detachable Boom Mic which also enables rotary Volume Control and Mute Switch. SteelFlex Arm for perfect microphone positioning. INPUT COMPATIBILITY - Devices supporting 3.5mm audio output such as gaming PS4 / Xbox One controller, PC, Laptop, iPhone and Android Phone.",
+      "Orange Fleur is a vibrant and energizing fragrance that blends the zesty freshness of orange with the delicate sweetness of orange blossom. This bright, floral scent captures the essence of a sun-drenched citrus grove, offering a refreshing, uplifting experience that lingers with a touch of elegance.",
     ratings: 4.1,
     images: [
       {
-        public_id: "shopit/demo/xmndneguqtrcutpf9wcz",
-        url: "http://res.cloudinary.com/udemy-courses/image/upload/v1698577572/shopit/demo/xmndneguqtrcutpf9wcz.jpg",
+        url: "https://res.cloudinary.com/taipham232/image/upload/v1737498983/ShopHaven/products/bottle-1.0_z1eche",
+      },
+      {
+        url: "https://res.cloudinary.com/taipham232/image/upload/v1737498983/ShopHaven/products/bottle-1.1_q9uqtz",
       },
     ],
-    category: "Accessories",
-    seller: "Amazon",
-    stock: 1123,
-    numOfReviews: 6,
+    category: "Fragrance",
+
+    stock: 12,
+    numOfReviews: 0,
     reviews: [],
   },
   {
-    name: "Nikon D3500 W/ AF-P DX NIKKOR 18-55mm f/3.5-5.6G VR Black",
-    price: 496.95,
+    name: "Argent 16",
+    price: 22.99,
     description:
-      "You don't need to be a photographer to know a great photo when you see one. And you don't need to be a photographer to take a great photo—you just need the D3500. It's as easy to use as a point-and-shoot, but it takes beautiful DSLR photos and videos that get noticed. It feels outstanding in your hands, sturdy and balanced with controls where you want them. It's compact, durable and versatile, ideal for travel. And it works seamlessly with compatible smartphones, making it easier than ever to share your great photos. Even if you've never picked up a DSLR camera, you can take beautiful pictures with D3500.",
+      "Argent is a sleek and sophisticated fragrance that embodies the cool elegance of silver. With crisp, metallic notes and a hint of fresh citrus and soft florals, it evokes a sense of modern luxury and timeless refinement, leaving a lasting impression of understated beauty.",
     ratings: 4.7,
     images: [
       {
         public_id: "shopit/demo/q6ybe84qlkzwet9qwptz",
-        url: "http://res.cloudinary.com/udemy-courses/image/upload/v1698577585/shopit/demo/q6ybe84qlkzwet9qwptz.jpg",
-      },
-      {
-        public_id: "shopit/demo/niqqio1faynnscubkkmd",
-        url: "http://res.cloudinary.com/udemy-courses/image/upload/v1698577585/shopit/demo/niqqio1faynnscubkkmd.jpg",
-      },
-      {
-        public_id: "shopit/demo/dy3nhjgsdgqcnrwf8u2h",
-        url: "http://res.cloudinary.com/udemy-courses/image/upload/v1698577585/shopit/demo/dy3nhjgsdgqcnrwf8u2h.jpg",
+        url: "https://res.cloudinary.com/taipham232/image/upload/v1737498983/ShopHaven/products/bottle-skinny-3.0_w1n6oy",
       },
     ],
-    category: "Cameras",
-    seller: "Amazon",
-    stock: 131,
-    numOfReviews: 222,
+    category: "Fragrance",
+  
+    stock: 10,
+    numOfReviews: 0,
     reviews: [],
   },
   {
-    name: "Apple MacBook Air (13-inch, 8GB RAM, 256GB SSD Storage) - Space Gray",
-    price: 949.99,
+    name: "Lavende 31",
+    price: 24.99,
     description:
-      "Stunning 13.3-inch Retina display with True Tone technology, Backlit Magic Keyboard and Touch ID, Tenth-generation, Intel Core i3 processor, Intel Iris Plus Graphics, Fast SSD storage, 8GB of memory, Stereo speakers with wider stereo sound",
+      "A serene and enchanting fragrance that blends calming lavender with hints of herbal freshness. Its soft, floral notes create a tranquil, spa-like atmosphere, while subtle woody undertones add depth, making it the perfect scent for relaxation and mindfulness.",
     ratings: 4.9,
     images: [
       {
-        public_id: "shopit/demo/jzqaj98nnhy0hcsilx9y",
-        url: "http://res.cloudinary.com/udemy-courses/image/upload/v1698577597/shopit/demo/jzqaj98nnhy0hcsilx9y.jpg",
+        url: "https://res.cloudinary.com/taipham232/image/upload/v1737498983/ShopHaven/products/bottle-skinny-1.1_etrkum",
+
       },
       {
-        public_id: "shopit/demo/welkq4dgfi5267usmj0n",
-        url: "http://res.cloudinary.com/udemy-courses/image/upload/v1698577597/shopit/demo/welkq4dgfi5267usmj0n.jpg",
-      },
-      {
-        public_id: "shopit/demo/pabtjloyzenmr6z8klcr",
-        url: "http://res.cloudinary.com/udemy-courses/image/upload/v1698577600/shopit/demo/pabtjloyzenmr6z8klcr.jpg",
+        url: "https://res.cloudinary.com/taipham232/image/upload/v1737498983/ShopHaven/products/bottle-skinny-1.0_arm3dj",
+
       },
     ],
-    category: "Laptops",
-    seller: "Amazon",
-    stock: 0,
-    numOfReviews: 56,
+    category: "Fragrance",
+
+    stock: 5,
+    numOfReviews: 0,
     reviews: [],
   },
   {
-    name: "Kauffman Orchards Homegrown McIntosh Apples",
+    name: "Lavande (spice)",
     price: 2.75,
     description:
-      "McIntosh is a pretty red/green apple with white flesh Homegrown in Kauffman's orchard in healthy Lancaster County soil McIntosh apples are famous for snacking, pies, salads, and applesauce Protected with soft, high-density foam for safe shipping to your door Not for sale to California, Oregon, or Washington due to state laws governing fresh produce",
+      "A twist on Lavande 31, spice is a captivating blend of soothing lavender and warm, aromatic spices. The fresh, floral lavender meets the boldness of cinnamon and clove, creating a balanced yet dynamic fragrance that’s both calming and invigorating—perfect for those seeking a unique, comforting scent with a touch of intrigue.",
     ratings: 5,
     images: [
       {
-        public_id: "shopit/demo/mq0zpyzmzqokphs9m9we",
-        url: "http://res.cloudinary.com/udemy-courses/image/upload/v1698577619/shopit/demo/mq0zpyzmzqokphs9m9we.jpg",
+        url: "https://res.cloudinary.com/taipham232/image/upload/v1737498983/ShopHaven/products/bottle-7.0_c6or4a",
       },
-      {
-        public_id: "shopit/demo/pkyy8rifxpesiwpy23kk",
-        url: "http://res.cloudinary.com/udemy-courses/image/upload/v1698577619/shopit/demo/pkyy8rifxpesiwpy23kk.jpg",
-      },
+     
     ],
-    category: "Food",
-    seller: "Kauffman's Fruit Farm & Market",
-    stock: 500,
-    numOfReviews: 1,
+    category: "Fragrance",
+
+    stock: 12,
+    numOfReviews: 0,
+    reviews: [],
+  },
+
+  {
+    name: "Amber & Willow",
+    price: 2.75,
+    description:
+      "A twist on Lavande 31, spice is a captivating blend of soothing lavender and warm, aromatic spices. The fresh, floral lavender meets the boldness of cinnamon and clove, creating a balanced yet dynamic fragrance that’s both calming and invigorating—perfect for those seeking a unique, comforting scent with a touch of intrigue.",
+    ratings: 5,
+    images: [
+      {
+        url: "https://res.cloudinary.com/taipham232/image/upload/v1737498983/ShopHaven/products/bottle-7.0_c6or4a",
+      },
+     
+    ],
+    category: "Candle",
+
+    stock: 12,
+    numOfReviews: 0,
+    reviews: [],
+  },
+
+  {
+    name: "Lavande (spice)",
+    price: 2.75,
+    description:
+      "A twist on Lavande 31, spice is a captivating blend of soothing lavender and warm, aromatic spices. The fresh, floral lavender meets the boldness of cinnamon and clove, creating a balanced yet dynamic fragrance that’s both calming and invigorating—perfect for those seeking a unique, comforting scent with a touch of intrigue.",
+    ratings: 5,
+    images: [
+      {
+        url: "https://res.cloudinary.com/taipham232/image/upload/v1737498983/ShopHaven/products/bottle-7.0_c6or4a",
+      },
+     
+    ],
+    category: "Candle",
+
+    stock: 12,
+    numOfReviews: 0,
+    reviews: [],
+  },
+
+  {
+    name: "Lavande (spice)",
+    price: 2.75,
+    description:
+      "A twist on Lavande 31, spice is a captivating blend of soothing lavender and warm, aromatic spices. The fresh, floral lavender meets the boldness of cinnamon and clove, creating a balanced yet dynamic fragrance that’s both calming and invigorating—perfect for those seeking a unique, comforting scent with a touch of intrigue.",
+    ratings: 5,
+    images: [
+      {
+        url: "https://res.cloudinary.com/taipham232/image/upload/v1737498983/ShopHaven/products/bottle-7.0_c6or4a",
+      },
+     
+    ],
+    category: "Candle",
+
+    stock: 12,
+    numOfReviews: 0,
+    reviews: [],
+  },
+
+  {
+    name: "Lavande (spice)",
+    price: 2.75,
+    description:
+      "A twist on Lavande 31, spice is a captivating blend of soothing lavender and warm, aromatic spices. The fresh, floral lavender meets the boldness of cinnamon and clove, creating a balanced yet dynamic fragrance that’s both calming and invigorating—perfect for those seeking a unique, comforting scent with a touch of intrigue.",
+    ratings: 5,
+    images: [
+      {
+        url: "https://res.cloudinary.com/taipham232/image/upload/v1737498983/ShopHaven/products/bottle-7.0_c6or4a",
+      },
+     
+    ],
+    category: "Candle",
+
+    stock: 12,
+    numOfReviews: 0,
+    reviews: [],
+  },
+
+  {
+    name: "Lavande (spice)",
+    price: 2.75,
+    description:
+      "A twist on Lavande 31, spice is a captivating blend of soothing lavender and warm, aromatic spices. The fresh, floral lavender meets the boldness of cinnamon and clove, creating a balanced yet dynamic fragrance that’s both calming and invigorating—perfect for those seeking a unique, comforting scent with a touch of intrigue.",
+    ratings: 5,
+    images: [
+      {
+        url: "https://res.cloudinary.com/taipham232/image/upload/v1737498983/ShopHaven/products/bottle-7.0_c6or4a",
+      },
+     
+    ],
+    category: "Candle",
+
+    stock: 12,
+    numOfReviews: 0,
     reviews: [],
   },
 ];
